80e6842855c201fedd4ef000b42115b7abebdbbc
Alan Ma <alan.ma@disney.com>
2014-07-20 13:48:48 -0700

    reverted back to the original default values

--- src/main/java/com/zanox/vertx/mods/internal/KafkaProperties.java
+++ src/main/java/com/zanox/vertx/mods/internal/KafkaProperties.java
@@ -31,9 +31,9 @@ public final class KafkaProperties {
     public static final String KAFKA_PARTITION = "kafka-partition";
 
 
-    public static final String DEFAULT_TOPIC = "kafka-rock";
+    public static final String DEFAULT_TOPIC = "default-topic";
     public static final String DEFAULT_PARTITION = "default-partition";
-    public static final String DEFAULT_BROKER_LIST = "192.168.86.10:9092";
+    public static final String DEFAULT_BROKER_LIST = "localhost:9092";
     public static final String DEFAULT_REQUEST_ACKS = "1";
     public static final String DEFAULT_KEY_SERIALIZER_CLASS = "kafka.serializer.StringEncoder";
 }
